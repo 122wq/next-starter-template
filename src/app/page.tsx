4cc8e0075c1f94a3ebc864321295c43a4ed75385
122wq <jackwanghaoran@outlook.com>
2026-02-15 21:21:25 -0500

    modified:   .vscode/settings.json modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
 		</div>
 	);
 }*/
-import Hello from "@/app/components/hello";
+import Hello from "../../components/hello";
 const Home = () => {
 	console.log("Hello, Next.js!");
   	return (
